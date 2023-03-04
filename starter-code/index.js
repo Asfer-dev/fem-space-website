2df1b987e5d8bdef1e1bbb0d130d0ebb6b90b36b
Asfer Ali <asferali004@gmail.com>
2023-03-05 01:57:02 +0500

    added the last page
    completed the websitegit status

--- starter-code/index.js
+++ starter-code/index.js
@@ -3,16 +3,17 @@ const iconHamburger = document.querySelector('#icon-hamburger');
 const iconClose = document.querySelector('#icon-close');
 const topNav = document.querySelector('.top-nav');
 
-
 navToggle.addEventListener('click', function() {
     topNav.toggleAttribute('data-visible');
 
     if (topNav.hasAttribute('data-visible')) {
         iconHamburger.setAttribute('data-visible', 'false');
         iconClose.setAttribute('data-visible', 'true');
+
     } else {
         iconHamburger.setAttribute('data-visible', 'true');
         iconClose.setAttribute('data-visible', 'false');
+
     }
 });
 
@@ -38,6 +39,24 @@ btnTab.forEach((btn) => {
         const selectedTab = document.querySelector('#details-' + btn.id);
         selectedTab.setAttribute('data-visible', 'true');
 
-        imgTab.src = `assets/${btn.getAttribute('data-source')}.png`;
+        if (document.body.classList[0] === 'technology') {
+            let orient = '';
+            if (getDeviceWidth() < 800) {
+                orient = 'landscape';
+            } else {
+                orient = 'portrait';
+            }
+            imgTab.src = `assets/${btn.getAttribute('data-source')}${orient}.jpg`;
+        } else {
+            imgTab.src = `assets/${btn.getAttribute('data-source')}`;
+        }
+
     });
 });
+
+
+
+
+function getDeviceWidth() {
+    return document.documentElement.clientWidth;
+}
